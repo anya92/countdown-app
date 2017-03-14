63bff0f31279a1d976446e92ca99b3c19b469a80
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-03-14 19:24:35 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const { resolve } = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'react'] }
+            }]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        })
+      }
+    ]
+  },
+  devServer: {
+    contentBase: resolve(__dirname, 'dist'),
+    compress: true,
+    stats: "errors-only",
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      hash: true,
+      inject: 'body'
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+};
+
+process.noDeprecation = true;
